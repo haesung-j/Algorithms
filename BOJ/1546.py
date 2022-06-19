200087fe24f12ce3bbc78287ed7b0930de6566f3
Jung HaeSung <47179168+haesung-j@users.noreply.github.com>
2022-06-19 15:33:00 +0900

    평균

--- BOJ/1546.py
+++ BOJ/1546.py
@@ -1,23 +1,7 @@
-N, M, K = map(int, input().split())
-array = input().split()
+N = int(input())
+scores = list(map(int,input().split()))
 
-for i in range(len(array)):
-    array[i] = int(array[i])
-  
-# N은 배열 크기, M: 더해지는 횟수, K: 연속 최대등장횟수
+max_s = max(scores)
+mean_s = sum(scores)/len(scores)
 
-array.sort(reverse=True)
-sum = 0
-k = 0
-
-for _ in range(M):
-  k += 1
-  if k == K:
-    sum += array[1]
-    k = 0
-  else:
-    sum += array[0]
-    
-  
-print(sum)
-print(6*3 + 5 + 6*3 + 5)
+print(mean_s/max_s * 100)
