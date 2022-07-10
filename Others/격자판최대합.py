1ff39d2824ec3fd804184a1373ceb3a2f508de64
Jung HaeSung <47179168+haesung-j@users.noreply.github.com>
2022-07-10 21:48:07 +0900

    Update 격자판최대합.py

--- Others/격자판최대합.py
+++ Others/격자판최대합.py
@@ -1,10 +1,9 @@
 n = int(input())
-
 li = []
-
 for _ in range(n):
     li.append(list(map(int, input().split())))
 
+# Solve 1
 sums = []
 s = 0
 for i in range(n):
@@ -27,3 +26,31 @@ sums.append(s)
 
 print(max(sums))
 
+## Solve 2
+# sums = []
+# max_sum = -1
+# for i in range(n):
+#     row_sum = 0
+#     col_sum = 0
+#     for j in range(n):
+#         row_sum += li[i][j]
+#         col_sum += li[j][i]
+#     if row_sum > max_sum:
+#         max_sum = row_sum
+        
+#     if col_sum > max_sum:
+#         max_sum = col_sum
+    
+# sum_diag1 = 0
+# sum_diag2 = 0
+# for i in range(n):
+#     sum_diag1 += li[i][i]
+#     sum_diag2 += li[i][n-1 - i]
+    
+# if sum_diag1 > max_sum:
+#     max_sum = sum_diag1
+    
+# if sum_diag2 > max_sum:
+#     max_sum = sum_diag2
+    
+# print(max_sum)  
